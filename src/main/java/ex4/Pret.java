b643ee7d1a889b100ecf868a93d2dba70a59474a
Léo SENIQUE <leo.senique@gmail.com>
2020-06-24 17:49:14 +0200

    modifié constructeurs pour exo 4

--- src/main/java/ex4/Pret.java
+++ src/main/java/ex4/Pret.java
@@ -12,7 +12,6 @@ public class Pret {
 	private Date dateFin;
 	
 	public Pret(double capital, double taux, Date dateDebut) {
-		super();
 		this.strategy = new PretATerme();
 		this.capital = capital;
 		this.taux = taux;
@@ -20,22 +19,13 @@ public class Pret {
 	}
 	
 	public Pret(double capital, double taux, Date dateDebut, Date dateFin) {
-		super();
-		this.strategy = new PretATerme();
-		this.capital = capital;
-		this.taux = taux;
-		this.dateDebut = dateDebut;
+		this(capital,taux,dateDebut);
 		this.dateFin = dateFin;
 	}
 	
 	public Pret(double capital, double capitalRestantDu, double taux, Date dateDebut, Date dateFin) {
-		super();
+		this(capital,taux,dateDebut,dateFin);
 		this.strategy = new PretATerme();
-		this.capital = capital;
-		this.capitalRestantDu = capitalRestantDu;
-		this.taux = taux;
-		this.dateDebut = dateDebut;
-		this.dateFin = dateFin;
 	}
 
 	/** Getter for strategy
